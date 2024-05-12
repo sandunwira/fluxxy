e53350ae47d6f6e92763bcb856f2dd37bb331edd
sandunwira <79461263+sandunwira@users.noreply.github.com>
2024-05-12 23:53:10 +0530

    Updated Styles

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+var favicon = require('serve-favicon');
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable for port
+const server = http.createServer(app);
+const io = socketio(server);
 
-// Serve static assets from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+// Favicon Start
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'images', 'logos', 'logo.png')));
+// Favicon End
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+
+let onlineCount = 0;
+
+io.on('connection', (socket) => {
+	console.log('New WebSocket Connection');
+	onlineCount++;
+	io.emit('onlineCount', onlineCount);
+
+	socket.on('chatMessage', (msg) => {
+		io.emit('chatMessage', msg);
+	});
+
+	socket.on('disconnect', () => {
+		onlineCount--;
+		io.emit('onlineCount', onlineCount);
+	});
+
+	console.log('Online Users:', onlineCount);
+});
+
 
 // Serve the service worker file
 app.get('/service-worker.js', (req, res) => {
@@ -17,6 +54,7 @@ app.get('/manifest.json', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'manifest.json'));
 });
 
-app.listen(port, () => {
-	console.log(`Server listening on port http://localhost:${port}`);
+
+server.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
 });
---
+++ public/js/chatroom.js
@@ -0,0 +1,59 @@
+const socket = io();
+
+const chatroom = document.getElementById('chatroom');
+const messageForm = document.getElementById('messageForm');
+const messageInput = document.getElementById('messageInput');
+const onlineUsers = document.getElementById('onlineUsers');
+
+messageInput.focus();
+
+// Listen for chat message events from the server
+socket.on('chatMessage', (msg) => {
+	const div = document.createElement('div');
+
+	// Check if the message is from the current user
+	if (msg.userId === socket.id) {
+		div.classList.add('my-msg');
+	} else {
+		div.classList.add('other-msg');
+	}
+
+	// Create a new span element for the message content
+	const msgContent = document.createElement('span');
+	msgContent.textContent = msg.text;
+	msgContent.classList.add('msg-content');
+
+	// Append the message content to the message div
+	div.appendChild(msgContent);
+
+	// Create a new span element for the timestamp
+	const timestamp = document.createElement('span');
+	timestamp.textContent = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+	timestamp.classList.add('timestamp');
+
+	// Append the timestamp to the message div
+	div.appendChild(timestamp);
+
+	chatroom.appendChild(div);
+
+	// Scroll to the bottom of the chat room
+	chatroom.scrollTop = chatroom.scrollHeight;
+});
+
+// Listen for onlineCount events from the server
+socket.on('onlineCount', (count) => {
+	if (count <= 1) {
+		onlineUsers.textContent = `${count} User Online`;
+	} else {
+		onlineUsers.textContent = `${count} Users Online`;
+	}
+});
+
+// Emit chat message events to the server
+messageForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const msg = messageInput.value;
+	// Include the user ID with the message
+	socket.emit('chatMessage', { text: msg, userId: socket.id });
+	messageInput.value = '';
+});
